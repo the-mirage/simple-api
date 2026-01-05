8e12de0286ff3dc123bdcecef87ac9d9158c550c
Brayarn Ogeda <brayarnogeda@gmail.com>
2026-01-05 09:23:37 +0300

    make http requests readable and create API endpoints

--- src/server.js
+++ src/server.js
@@ -1,11 +1,27 @@
 import express from "express";
-import bodyParser from "body-parser";
+import { StatusCodes } from "http-status-codes";
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.json()); // Middleware to parse JSON bodies
+
 app.get("/", (req, res) => {
   res.send("Hola, como estas?");
+});
+
+// Add post request handler
+app.post("/add", (req, res) => {
+  const data = [];
+  const { body } = req;
+  if (!body.name) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .send({ message: "Name is required", data });
+  }
+  return res
+    .status(StatusCodes.CREATED)
+    .send({ message: "Added successfully", data });
 });
 
 app.listen(PORT, () => {
